eae34541edf3f0395c4e2575a45669c9be09d1b8
Teo <tranquocdungb4@gmail.com>
2023-03-26 19:29:07 +0700

    update dashbord

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,7 @@ import Background from './Background';
 import styles from './Dashboard.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTemperatureHalf, faDroplet, faHouse } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -11,30 +12,36 @@ function DashBoard() {
     return (
         <div className={cx('wrapper')}>
             <Background />
-            <Box
-                style={{
-                    background: 'var(--temp)',
-                }}
-                leftIcon={<FontAwesomeIcon className={cx('icon')} icon={faTemperatureHalf} />}
-            >
-                27&#8451;
-            </Box>
-            <Box
-                style={{
-                    background: 'var(--hud)',
-                }}
-                leftIcon={<FontAwesomeIcon className={cx('icon')} icon={faDroplet} />}
-            >
-                35%
-            </Box>
-            <Box
-                style={{
-                    background: 'var(--home)',
-                }}
-                leftIcon={<FontAwesomeIcon className={cx('icon')} icon={faHouse} />}
-            >
-                Good
-            </Box>
+            <Link to={'/sensordata'}>
+                <Box
+                    style={{
+                        background: 'var(--temp)',
+                    }}
+                    leftIcon={<FontAwesomeIcon className={cx('icon')} icon={faTemperatureHalf} />}
+                >
+                    27&#8451;
+                </Box>
+            </Link>
+            <Link to={'/sensordata'}>
+                <Box
+                    style={{
+                        background: 'var(--hud)',
+                    }}
+                    leftIcon={<FontAwesomeIcon className={cx('icon')} icon={faDroplet} />}
+                >
+                    35%
+                </Box>
+            </Link>
+            <Link to={'/sensordata'}>
+                <Box
+                    style={{
+                        background: 'var(--home)',
+                    }}
+                    leftIcon={<FontAwesomeIcon className={cx('icon')} icon={faHouse} />}
+                >
+                    Good
+                </Box>
+            </Link>
         </div>
     );
 }
