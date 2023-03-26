a408a4db09d441b9b73f014bd92d2afc78e85c8e
Teo <tranquocdungb4@gmail.com>
2023-03-26 16:27:59 +0700

    noti

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,5 +1,6 @@
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
@@ -9,7 +10,25 @@ function Header() {
     return (
         <div className={cx('wrapper')}>
             <div>
-                <FontAwesomeIcon className={cx('icon')} icon={faBell} />
+                <Tippy
+                    interactive
+                    placement="bottom-start"
+                    render={(attrs) => (
+                        <div className={cx('list')} tabIndex="-1" {...attrs}>
+                            <div className={cx('noti')}>
+                                <div className={cx('content')}>
+                                    <h3>Tình trạng hôm nay</h3>
+                                    <p>Nhiệt độ: 27oC. Độ ẩm: 35%. Tình trạng chung: Good</p>
+                                </div>
+                                <div className={cx('foot')}>
+                                    <p>1 giờ trước</p>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                >
+                    <FontAwesomeIcon className={cx('icon')} icon={faBell} />
+                </Tippy>
             </div>
         </div>
     );
